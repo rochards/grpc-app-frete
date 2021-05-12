f135f7322849c5ca25f7ba6605388d4b57a372fe
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-05-12 19:43:46 -0300

    Retornando status de erro customizado no lado do servidor parte 1

--- frete/src/main/kotlin/br/com/zupacademy/frete/FretesGrpcServer.kt
+++ frete/src/main/kotlin/br/com/zupacademy/frete/FretesGrpcServer.kt
@@ -1,7 +1,11 @@
 package br.com.zupacademy.frete
 
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.IllegalStateException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -14,9 +18,38 @@ class FreteGrpcServer : FreteServiceGrpc.FreteServiceImplBase() {
 
         logger.info("calculando frete para request - $request")
 
+        val cep = request?.cep
+        if (cep.isNullOrBlank()) {
+            val error = Status.INVALID_ARGUMENT
+                .withDescription("cep deve ser informado")
+                .asRuntimeException()
+            responseObserver?.onError(error)
+        } else if (!cep.matches("[0-9]{5}-[0-9]{3}".toRegex())) {
+            val error = Status.INVALID_ARGUMENT
+                .withDescription("cep deve estar em formato válido")
+                .augmentDescription("formato esperado deve ser 99999-999")
+                .asRuntimeException()
+            responseObserver?.onError(error)
+        }
+
+        var randomValue = 0.0
+        try {
+            // esse try é só para simular como tratar um possível error qualquer
+            randomValue = Random.nextDouble(from = 0.0, until = 140.0)
+            if (randomValue > 100) {
+                throw IllegalStateException("Erro aleatório de regra de negócio!")
+            }
+        } catch (e: Exception) {
+            responseObserver?.onError(
+                Status.INTERNAL
+                    .withDescription(e.message)
+                    .withCause(e) // anexado ao Status, mas não enviado ao Client
+                    .asRuntimeException()
+            )
+        }
         val response = CalculaFreteResponse.newBuilder()
-            .setCep(request?.cep)
-            .setValor(Random.nextDouble(from = 0.0, until = 140.0))
+            .setCep(cep)
+            .setValor(randomValue)
             .build()
 
         logger.info("frete calculado - $response")
